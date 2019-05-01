a1e0b5a6e17dc0cfbf780f4b1f3cf652265693d9
Jessica Tribble <jessicablanchard@Jessicas-MacBook-Pro.local>
2019-05-01 11:49:37 -0400

    Assignment work - incremented id variable, added delete button in renderTheUi, removed to-do from array

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo){
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const DELETE_BTN = document.createElement('button')
+      DELETE_BTN.textContent = "Delete";
+      DELETE_BTN.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+            })
+
+        renderTheUI();
+          });
+
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(DELETE_BTN);
+    });
+  }
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
     toDos.push({
       title: newToDoText.value,
-      complete: false
-    });
-    newToDoText.value = '';
-    renderTheUI();
-  }
+      complete: false,
+      id: id
+      });
 
-  function renderTheUI() {
-    const toDoList = document.getElementById('toDoList');
-
-    toDoList.textContent = '';
-
-    toDos.forEach(function(toDo) {
-      const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
-
-      newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-    });
-  }
+      id++;
+      newToDoText.value = '';
+      renderTheUI();
+    }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
